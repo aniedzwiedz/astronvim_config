2853389d98760069cafb05b1e843642920f50484
Niedzwiedz Artur <artur.niedzwiedz@igt.com>
2023-03-30 00:11:03 +0200

    more

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,31 +1,59 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  -- TODO:  test
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    -- config = function()
-    --   require("todo-comments").setup {}
-    -- end
-    opts = {},
-    event = "User AstroFile",
-    cmd = { "TodoQuickFix" },
-    keys = {
-      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
-    },
-  },
-  {
-    "xiyaowong/transparent.nvim",
-    event = "VeryLazy",
-    config = function() vim.cmd [[TransparentEnable]] end,
-  },
+	-- You can also add new plugins here as well:
+	-- Add plugins, the lazy syntax
+	-- "andweeb/presence.nvim",
+	-- {
+	--   "ray-x/lsp_signature.nvim",
+	--   event = "BufRead",
+	--   config = function()
+	--     require("lsp_signature").setup()
+	--   end,
+	-- },
+	-- TODO:  test
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		-- config = function()
+		--   require("todo-comments").setup {}
+		-- end
+		opts = {},
+		event = "User AstroFile",
+		cmd = { "TodoQuickFix" },
+		keys = {
+			{ "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+		},
+	},
+	{
+		"xiyaowong/transparent.nvim",
+		event = "VeryLazy",
+		config = function() vim.cmd [[TransparentEnable]] end,
+	},
+	-- { "bluz71/vim-nightfly-guicolors" },
+	{ "rodjek/vim-puppet" },
+	{ "pearofducks/ansible-vim" },
+	{
+		"phaazon/hop.nvim",
+		branch = "v2",
+		event = "BufRead",
+		config = function()
+			require("hop").setup()
+			-- vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+			vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+		end,
+	},
+	-- {
+	-- 	"sindrets/diffview.nvim",
+	-- 	event = "User AstroFile",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 	},
+	-- },
+	-- {
+	-- 	"norcalli/nvim-colorizer.lua",
+	-- 	event = { "BufRead" },
+	-- 	config = function()
+	-- 		-- load nvim-colorizer
+	-- 		vim.cmd([[ColorizerAttachToBuffer]])
+	-- 	end,
+	-- },
 }
